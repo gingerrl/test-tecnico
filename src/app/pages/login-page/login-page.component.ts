e5a9dda20d58e6f5785f622ae933edef5443cb8c
unknown <ginger1881@hotmail.com>
2025-03-19 10:39:45 -0500

    feat: files pages login-page, main-page

---
+++ src/app/pages/login-page/login-page.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrl: './login-page.component.css'
+})
+export class LoginPageComponent {
+
+  constructor(
+    private readonly router: Router
+  ){}
+
+  onButtonInit(){
+    this.router.navigate(['/invoice-consult/home'])
+  }
+}
---
+++ src/app/pages/main-page/main-page.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html',
+  styleUrl: './main-page.component.css'
+})
+export class MainPageComponent {
+
+  constructor(
+    private readonly router : Router
+  ){
+
+  }
+
+  onButtonLogout(){
+    this.router.navigate(['/login'])
+  }
+}
